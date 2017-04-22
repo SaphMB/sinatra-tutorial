eab131e3cc17c6a8a753302fc9225dbf2e777cde
Sapphire <saphmb@gmail.com>
2017-04-22 12:43:02 +0100

    created database

--- webapp.rb
+++ webapp.rb
@@ -1,37 +1,33 @@
 require 'sinatra'
 require 'data_mapper'
 
-Datamapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/dev.db")
+DataMapper::setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/dev.db")
 
 class Message
-	include DataMapper::Resource
-	property :id, Serial
-	property :text, String
+  include DataMapper::Resource
+  property :id, Serial
+  property :text, String
 end
 
 DataMapper.finalize
 Message.auto_upgrade!
 
 get '/' do
-  'Hello, World!'
+ 'Hello, World!'
+end
+
+get '/hello/:name' do
+  "Hello there, #{params[:name]}."
 end
 
 get '/hello/:name/:city' do
-	"Hello there, #{params[:name]} from #{params[:city]}."
+	"Hey there #{params[:name]} from #{params[:city]}."
 end
 
 get '/more/*' do
-	params[:splat]
+  params[:splat]
 end
 
-get '/form' do
-	erb :form
-end
-
-#post '/form' do
-#	'Hello again'
-#end
-
-post '/form' do
-	"You submitted this '#{params[:message]}' "
+get '/messages/new' do
+ erb :form
 end
